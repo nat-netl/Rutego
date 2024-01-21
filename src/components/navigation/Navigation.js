c80d6a274337dcac27cad6836ced6e9e98b93951
nat-netl <71555737+nat-netl@users.noreply.github.com>
2024-01-22 02:18:28 +0300

    eddits

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -6,7 +6,7 @@ import rutegoImg from "./../../assets/img/rutego.svg";
 function Navigation(props) {
     console.log (props.openMenu)
   return (
-    <div className={`${s.navigation} ${props.openMenu == false ? s.close : s.open}`}>
+    <div className={`${s.navigation} ${props.openMenu === false ? s.close : s.open}`}>
       <div className={s.navigation__container}>
         <button onClick={() => props.setOpenMenu(false)} type="button" className={s.navigation__close}></button>
         <div className={s.navigation__wrapper}>
