dcfb03b92c669438189772ee42f991a168aee7c7
nat-netl <71555737+nat-netl@users.noreply.github.com>
2024-01-21 14:24:17 +0300

    possibilities component

--- src/components/possibilities/Possibilities.js
+++ src/components/possibilities/Possibilities.js
@@ -3,40 +3,47 @@ import s from "./possibilities.module.scss";
 
 const Possibilities = () => {
   return (
-    <div className={s.possibilities}>
+    <section className={s.possibilities}>
       <div className={s.possibilities__container}>
+        <div className={s.figure__bottom}></div>
+        <div className={s.figure__left}></div>
+
+
         <div className={s.possibilities__wrapper}>
           <div className={s.possibilities_wrapper__items}>
             <div className={s.possibilities_item1}>
-              <p>cложный код</p>
+              <div>cложный код</div>
               <p>корпоративные</p>
             </div>
             <div className={s.possibilities_item2}>
-              <p>landing-page</p>
+              <div>landing-page</div>
+              <p>Выгодно</p>
             </div>
             <div className={s.possibilities_item3}>
-              <p>интернет магазины</p>
+              <div>интернет магазины</div>
+              <p></p>
             </div>
             <div className={s.possibilities_item4}>
-              <p>интеграции</p>
-              <small>
+              <div className={s.item4}><div>интеграции</div></div>
+              <p>
                 Наша компания занимается разработкой программного обеспечения <br />
                 и веб-сайтов. Мы предлагаем широкий спектр услуг, включая <br />
                 создание сайтов, мобильных приложений, разработку веб <br />
                 -приложений, интеграцию с другими сервисами и многое другое.
-              </small>
+              </p>
             </div>
             <div className={s.possibilities_item5}>
-              <p>доработки</p>
+              <div>доработки</div>
               <p>AND</p>
             </div>
             <div className={s.possibilities_item6}>
-              <p>маркетинг</p>
+              <div>маркетинг</div>
+              <p></p>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
