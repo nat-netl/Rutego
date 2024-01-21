19b8286e458f256e9f7bbc7b2ccffcd2353d7bc3
nat-netl <71555737+nat-netl@users.noreply.github.com>
2024-01-21 22:56:59 +0300

    navigation component

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,66 @@
+import React from "react";
+import s from "./navigation.module.scss";
+import logoImg from "./../../assets/img/logo.svg";
+import rutegoImg from "./../../assets/img/rutego.svg";
+
+function Navigation(openMenu, setOpenMenu) {
+  return (
+    <div className={s.navigation}>
+      <div className={s.navigation__container}>
+        <div className={s.navigation__wrapper}>
+          <div className={s.navigation__logo}>
+            <img className={s.logo__img} src={logoImg} alt="" />
+            <img className={s.logo__img} src={rutegoImg} alt="" />
+          </div>
+
+          <div className={s.navigation__buttons}>
+            <button
+              className={s.navigation__button}
+              type="button"
+              value="Маркетинг"
+            >
+              <p className={s.button__icon}></p>
+              Маркетинг
+            </button>
+            <button
+              className={s.navigation__button}
+              type="button"
+              value="Разработка"
+            >
+              <p className={s.button__icon}></p>
+              Разработка
+            </button>
+            <button
+              className={s.navigation__button}
+              type="button"
+              value="Поддержка"
+            >
+              <p className={s.button__icon}></p>
+              Поддержка
+            </button>
+          </div>
+
+          <div className={s.navigationButtom__buttons}>
+            <button
+              className={s.navigationButtom__buttonPersonal}
+              type="button"
+              value="Личный кабинет"
+            >
+              <p className={s.button__iconPersonal}></p>
+              Личный кабинет
+            </button>
+            <button
+              className={s.navigationButtom__buttonOrder}
+              type="button"
+              value="заказать"
+            >
+              Заказать
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./sideBar.module.scss";
 import logo from "./../../assets/img/logo.png";
-import acivateSidebar from "./../../assets/img/sidebar.png";
+import Navigation from "../navigation/Navigation";
 
 const SideBar = () => {
-  return (
-    <div className={s.sidebar}>
-      <div className={s.sidebar__container}>
-        <div className={s.logo}>
-          <a className={s.logo__link} href="/">
-            <img className={s.logoLink__img} src={logo} alt="" />
-          </a>
-        </div>
+  const [openMenu, setOpenMenu] = useState(false);
 
-        <div className={s.acivateSidebar}>
-          <a className={s.acivateSidebar__link} href="/#" >
-            <img className={s.acivateSidebar__img} src={acivateSidebar} alt="" />
-          </a>
+  return (
+    <>
+      <div className={s.sidebar}>
+        <div className={s.sidebar__container}>
+          <div className={s.logo}>
+            <a className={s.logo__link} href="/">
+              <img className={s.logoLink__img} src={logo} alt="" />
+            </a>
+          </div>
+
+          <div className={s.acivateSidebar}>
+            <a className={s.acivateSidebar__link} href="/#" />
+          </div>
         </div>
       </div>
-    </div>
+      <Navigation openMenu={openMenu} setOpenMenu={setOpenMenu} />
+    </>
   );
 };
 
